d82d564ab04cb34aab422d4f7df280823c352681
Anton1273751 <kronoss161@gmail.com>
2024-01-26 16:10:07 +0300

    add func

--- style/app/index.js
+++ style/app/index.js
@@ -2,13 +2,22 @@ import { data } from "./emoji.js";
 const containerNode = document.querySelector(".container");
 const inputNode = document.querySelector(".header__plas");
 
+const delDobleStr = (data) => {
+  for (const { keywords } of data) {
+    const arr = keywords.split(" ");
+    const newSet = new Set(arr);
+    const newStr = Array.from(newSet).join(" ");
+    return newStr;
+  }
+};
+
 const createCards = ({ title, symbol, keywords }) => {
   const cardNode = document.createElement("div");
   cardNode.className = "card";
   cardNode.innerHTML = `
                         <text class = "card__picture"> ${symbol}</text>
                         <p class = "card__article">${title}</p>
-                        <p class = "card__text">${keywords} </p>
+                        <p class = "card__text">${delDobleStr(data)} </p>
                         `;
 
   return cardNode;
@@ -22,8 +31,15 @@ const renderCard = (data) => {
 inputNode.addEventListener("input", (e) => {
   const value = e.target.value;
   const filterCard = data.filter(
-    (el) => el.title.includes(value) || el.keywords.includes(value)
+    ({ title, keywords }) =>
+      title.toLowerCase().includes(value.toLowerCase()) ||
+      keywords.toLowerCase().includes(value.toLowerCase())
   );
   renderCard(filterCard);
 });
+
+console.log(delDobleStr(data));
+
 renderCard(data);
+
+// console.log(...data);
